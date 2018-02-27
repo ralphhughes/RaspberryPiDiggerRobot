9c7aa6855fb8c40789cc6417d85cd6d14ded30c0
Ralphius <ralphhughes@hotmail.com>
2018-02-27 22:34:04 +0000

    Split event handlers into seperate files Added mousewheel JS lib from CDN

---
+++ frontend/keyboard_events.js
@@ -0,0 +1,63 @@
+function addKeyboardHandlers() {
+    // Robot control event handlers below here:
+    $(document).keydown(function (e) {
+        console.log("keycode: " + e.keyCode);
+        // If escape or space is pressed, stop all motors
+        if (e.keyCode === 27 || e.keyCode === 32) {
+            tractionMotor = 0;
+            armMotor = 0;
+            sendMessage("a=0, t=0");
+            return false; //Stops the PgDown?
+        }
+
+        if (e.keyCode === 87) { // W: Forward
+            tractionMotor++;
+            tractionMotor = cropToRange(tractionMotor, -100, 100);
+            sendMessage("t=" + tractionMotor);
+        }
+        if (e.keyCode === 65) { // A: Left
+            steeringServo--;
+            steeringServo = cropToRange(steeringServo, -100, 100);
+            sendMessage("s=" + steeringServo);
+        }
+        if (e.keyCode === 83) { // S: Backwards
+            tractionMotor--;
+            tractionMotor = cropToRange(tractionMotor, -100, 100);
+            sendMessage("t=" + tractionMotor);
+        }
+        if (e.keyCode === 68) { // D: Right
+            steeringServo++;
+            steeringServo = cropToRange(steeringServo, -100, 100);
+            sendMessage("s=" + steeringServo);
+        }
+
+
+    });
+
+    // Event handler for changing bucket position when keys are held down
+    $(document).keydown(function (e) {
+        // 49-57 (Num keys 1-9) for bucket angle
+        if (e.keyCode >= 49 && e.keyCode <= 57) {
+            var numberPressed = e.keyCode - 48;
+            bucketServo = Math.round(((numberPressed - 1) / 8) * 100);
+            sendMessage("b=" + bucketServo);
+        }
+
+        // plus\minus keys for fine adjustment
+        if (e.keyCode === 173) {
+            bucketServo--;
+            bucketServo = cropToRange(bucketServo, 0, 100);
+            sendMessage("b=" + bucketServo);
+        }
+
+        if (e.keyCode === 61) {
+            bucketServo++;
+            bucketServo = cropToRange(bucketServo, 0, 100);
+            sendMessage("b=" + bucketServo);
+        }
+
+        // Don't propagate this event to other handlers
+        // return false;
+    });
+
+}
---
+++ frontend/mouse_events.js
@@ -0,0 +1,60 @@
+function addMouseHandlers() {
+    $('.overlays').on('mousewheel', function(event) {
+        console.log(event.deltaX, event.deltaY, event.deltaFactor);
+        bucketServo = bucketServo + event.deltaY;
+        bucketServo = cropToRange(bucketServo, 0, 100);
+        sendMessage("b=" + bucketServo);
+    });
+
+
+    // Event handler for controlling steering and traction motors dependent upon mouse position
+    $(".overlays").mousemove(function (e) {
+        var parentOffset = $(this).parent().offset();
+        var x = (e.pageX - parentOffset.left); //offset -> method allows you to retrieve the current position of an element 'relative' to the document
+        var y = (e.pageY - parentOffset.top);
+        x = Math.round(((x / VIEWPORT_WIDTH) * 120) - 10);
+        y = Math.round(((y / VIEWPORT_HEIGHT) * 250) - (250 / 2));
+
+        // Dead band round center of div to prevent traction motor stall under low duty cycle PWM
+        if (y > -15 && y < 15) {
+            y = 0;
+        }
+        // Full power band round edge of div to make it easier to hold mouse on 100%
+        if (x > 100) {
+            x = 100;
+        }
+        if (x < 0) {
+            x = 0;
+        }
+        
+        console.log("x:" + x);
+        sendMessage("c=" + cameraServo);
+    });
+
+    // Event handler for stopping traction and arm motors when mouse is not on the video stream
+    $(".overlays").mouseleave(function (e) {
+        tractionMotor = 0;
+        armMotor = 0;
+        // fire ajax call with motor stop signals 
+        sendMessage("t=0,a=0");
+    });
+
+    // Event handler for raising and lowering arm on left/right click
+    $(".overlays").mousedown(function (event) {
+        switch (event.which) {
+            case 1:
+                armMotor = 100;
+                break;
+            case 3:
+                armMotor = -100;
+                break;
+        }
+        sendMessage("a=" + armMotor);
+    });
+
+    // Event handler for stopping arm motor when mouse button no longer held down
+    $(".overlays").mouseup(function () {
+        armMotor = 0;
+        sendMessage("a=" + armMotor);
+    });
+}
