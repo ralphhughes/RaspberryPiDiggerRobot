ffae7ae265ef99c439a4401f1a0a25cf976996f1
Ralph <pi@rpi-robot>
2018-03-02 21:32:26 +0000

    Typo in webcam script fixed

--- backend/start_webcam.sh
+++ backend/start_webcam.sh
@@ -1,4 +1,5 @@
-export LB_LIBRARY_PATH=./mjpg-streamer/mjpg-streamer-experimental
+#!/bin/sh
+export LD_LIBRARY_PATH=./mjpg-streamer/mjpg-streamer-experimental
 
 # Medium quality
 # ./mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 480 -y 640 -rot 90 -fps 5 -quality 15"
