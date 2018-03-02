2c81bb0c95ba85cedb2dd945c9207c56c12ac1c3
Ralph <pi@rpi-robot>
2018-03-02 21:21:05 +0000

    Stupid win-unix line endings

--- backend/install_webcam_server.sh
+++ backend/install_webcam_server.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
+echo "Downloading mjpeg-streamer-experimental..."
+
 # Setup the webcam streaming server
 cd ~/RaspberryPiDiggerRobot/backend
-
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 
+echo "Installing mjpeg-streamer-experimental..."
 cd mjpg-streamer/mjpg-streamer-experimental
-
 make
-
 sudo make install
