9408ab1e3ebdc1debe4ccee6a355dfad1e9f2c0d
Ralph <pi@rpi-robot>
2018-03-02 23:01:45 +0000

    Rotated the camera stream

--- backend/start_webcam.sh
+++ backend/start_webcam.sh
@@ -2,7 +2,7 @@
 export LD_LIBRARY_PATH=./mjpg-streamer/mjpg-streamer-experimental
 
 # Medium quality
-# ./mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 480 -y 640 -rot 90 -fps 5 -quality 15"
+# ./mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 640 -y 480 -rot 180 -fps 5 -quality 15"
 
 # Poor quality
-./mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 384 -y 512 -rot 90 -vs -fps 5 -quality 10"
+./mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 512 -y 384 -rot 180 -vs -fps 5 -quality 10"
