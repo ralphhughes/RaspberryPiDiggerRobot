ceeb97037ba3e10deff6b2dfc4fc65842e2df6ba
ralph <ralph@HP-Pavilion-broken-screen.Home>
2018-08-24 17:38:42 +0100

    Manual merge of parts of patch from alt fork: https://github.com/adrhc/h264-live-player/commit/9721ed7ac580228e500eb44089207988784f8830

--- backend/_server.js
+++ backend/_server.js
@@ -18,6 +18,8 @@ class _Server {
         fps: 12
     }, options);
 
+    this.counter = 0;
+    
     this.wss = new WebSocketServer({ server });
 
     this.new_client = this.new_client.bind(this);
@@ -29,11 +31,17 @@ class _Server {
   
 
   start_feed() {
-    var readStream = this.get_feed();
+    this.streamer = this.get_feed();
+    var readStream = this.streamer.stdout;
     this.readStream = readStream;
-
     readStream = readStream.pipe(new Splitter(NALseparator));
     readStream.on("data", this.broadcast);
+    readStream.on("close", function () {
+      console.log("_server.js close");
+    });
+    readStream.on("error", function (code) {
+      console.log(`_server.js error code = ${code}`);
+    });
   }
 
   get_feed() {
@@ -42,7 +50,10 @@ class _Server {
 
   broadcast(data) {
     this.wss.clients.forEach(function(socket) {
-
+      if (socket.readyState !== WebSocket.OPEN) {
+        console.log('socket.readyState =', socket.readyState);
+        return;
+      }
       if(socket.buzy)
         return;
 
@@ -60,25 +71,39 @@ class _Server {
     var self = this;
     console.log('New guy');
 
+    if (!self.readStream) {
+      self.start_feed();
+    } else if (self.readStream.isPaused()) {
+      self.readStream.resume();
+    } else {
+      console.log(`new_client, streamer: connected = ${self.streamer.connected}, killed = ${self.streamer.killed}, .pid = ${self.streamer.pid}`);
+    }
+
     socket.send(JSON.stringify({
       action : "init",
       width  : this.options.width,
       height : this.options.height,
+      fps: this.options.fps
     }));
 
     socket.on("message", function(data){
       var cmd = "" + data, action = data.split(' ')[0];
       console.log("Incomming action '%s'", action);
 
-      if(action == "REQUESTSTREAM")
+      if (action == "STOPSTREAM" || (action == "REQUESTSTREAM" && !!self.streamer)) {
+        // self.streamer.kill('SIGKILL');
+        self.streamer.kill();
+        self.readStream = undefined;
+        console.log("previous streamer killed");
+      }
+      if (action == "REQUESTSTREAM") {
         self.start_feed();
-      if(action == "STOPSTREAM")
-        self.readStream.pause();
+      }
     });
 
     socket.on('close', function() {
-      self.readStream.end();
-      console.log('stopping client interval');
+      // self.readStream.end();
+      console.log('web-socket closed');
     });
   }
 
