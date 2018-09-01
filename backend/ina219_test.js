ba13ea3faf74f6c5a14b9cddb49d751b69844e6a
ralph <ralph@HP-Pavilion-broken-screen.Home>
2018-09-01 21:54:30 +0100

    ina219_test has better formatted whitespace ina219 have done used the "eliotlandrum" fork as that seems further ahead.

--- backend/ina219_test.js
+++ backend/ina219_test.js
@@ -1,18 +1,17 @@
+var ina219 = require('ina219');
 
-  var ina219 = require('ina219');
+ina219.init();
+ina219.enableLogging(true);
 
-  ina219.init();
-  ina219.enableLogging(true);
-  
-  ina219.calibrate32V1A(function () {
-    
+ina219.calibrate32V1A(function () {
+
     ina219.getBusVoltage_V(function (volts) {
-	  
-      console.log("Voltage: " + volts);
-      ina219.getCurrent_mA(function (current){
-  			
-        console.log("Current (mA): " + current );
-      });	
+
+        console.log("Voltage: " + volts);
+        ina219.getCurrent_mA(function (current) {
+
+            console.log("Current (mA): " + current);
+        });
     });
-  });
+});
 
