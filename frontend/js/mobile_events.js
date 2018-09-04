23e381c2e592ca60c95f92e609d17fa38933b724
ralph <ralph@HP-Pavilion-broken-screen.Home>
2018-09-04 11:51:26 +0100

    First attempt at adding twin virtual thumbsticks for touchscreen devices

--- frontend/js/mobile_events.js
+++ frontend/js/mobile_events.js
@@ -1,7 +1,21 @@
 "use strict";
 
 function addTouchEvents() {
-
+    $(function() {                                                                                                                                    
+        $('#stick1').joystick({                                                                                                                   
+                moveEvent: function(pos) { console.log('Joystick 1:' + pos.x + ', ' + pos.y); },                                                   
+                endEvent: function(pos) { console.log('Joystick 1:' + pos.x + ', ' + pos.y); }                                                     
+        });                                                                                                                                       
+        $('#stick2').joystick({                                                                                                                   
+                moveEvent: function(pos) { console.log('yaw:' + pos.x); },                                                                         
+                endEvent: function(pos) { console.log('yaw:' + pos.x); }                                                                           
+        });                                                                                                                                       
+                                                                                                                                                  
+        // Default positions                                                                                                                      
+        $('#stick1').joystick('value', 0.5, 0.5);                                                                                                 
+        $('#stick2').joystick('value', 0.5, 0.5);                                                                                                 
+                                                                                                                                                  
+});                                                                                                                                               
 }
 
 function addTiltEvents() {
