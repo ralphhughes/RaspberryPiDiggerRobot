00ad9a1faad3d1bc9511c453cc6e077e95c20955
ralph <ralph@HP-Pavilion-broken-screen.Home>
2018-09-04 12:10:49 +0100

    Joystick style bugfix

--- frontend/js/mobile_events.js
+++ frontend/js/mobile_events.js
@@ -1,21 +1,29 @@
 "use strict";
 
 function addTouchEvents() {
-    $(function() {                                                                                                                                    
-        $('#stick1').joystick({                                                                                                                   
-                moveEvent: function(pos) { console.log('Joystick 1:' + pos.x + ', ' + pos.y); },                                                   
-                endEvent: function(pos) { console.log('Joystick 1:' + pos.x + ', ' + pos.y); }                                                     
-        });                                                                                                                                       
-        $('#stick2').joystick({                                                                                                                   
-                moveEvent: function(pos) { console.log('yaw:' + pos.x); },                                                                         
-                endEvent: function(pos) { console.log('yaw:' + pos.x); }                                                                           
-        });                                                                                                                                       
-                                                                                                                                                  
+    $(function () {
+        $('#stick1').joystick({
+            moveEvent: function (pos) {
+                console.log('Joystick 1:' + pos.x + ', ' + pos.y);
+            },
+            endEvent: function (pos) {
+                console.log('Joystick 1:' + pos.x + ', ' + pos.y);
+            }
+        });
+        $('#stick2').joystick({
+            moveEvent: function (pos) {
+                console.log('yaw:' + pos.x);
+            },
+            endEvent: function (pos) {
+                console.log('yaw:' + pos.x);
+            }
+        });
+
         // Default positions                                                                                                                      
-        $('#stick1').joystick('value', 0.5, 0.5);                                                                                                 
-        $('#stick2').joystick('value', 0.5, 0.5);                                                                                                 
-                                                                                                                                                  
-});                                                                                                                                               
+        $('#stick1').joystick('value', 0.5, 0.5);
+        $('#stick2').joystick('value', 0.5, 0.5);
+
+    });
 }
 
 function addTiltEvents() {
@@ -31,11 +39,11 @@ function addTiltEvents() {
         if (beta > 90) {
             beta = 90;
         }
-        
+
         if (beta < -90) {
             beta = -90;
         }
-        
+
     }
 
     window.addEventListener('deviceorientation', handleOrientation);
