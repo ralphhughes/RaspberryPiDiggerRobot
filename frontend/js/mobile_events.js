43cb2c0117541c59687455a2522ee4902943db03
ralph <ralph@HP-Pavilion-broken-screen.Home>
2018-09-04 13:58:49 +0100

    First go at connecting the left joystick to its functions

--- frontend/js/mobile_events.js
+++ frontend/js/mobile_events.js
@@ -1,13 +1,34 @@
 "use strict";
 
 function addTouchEvents() {
+    window.addEventListener('touchstart', function onFirstTouch() {
+        // If the screen is touched, show the touch controls
+        $('#stick1').fadeIn('slow');
+        $('#stick2').fadeIn('slow');
+
+        // we only need to know once that a human touched the screen, so we can stop listening now
+        window.removeEventListener('touchstart', onFirstTouch, false);
+    }, false);
+
     $(function () {
         $('#stick1').joystick({
             moveEvent: function (pos) {
                 console.log('Joystick 1:' + pos.x + ', ' + pos.y);
+                tractionMotor = (pos.y - 0.5) * 2 * 255;
+                tractionMotor = setDeadband(tractionMotor, 50);
+                tractionMotor = cropToRange(tractionMotor, -255, 255);
+                sendMessage("t=" + tractionMotor);
+                
+                steeringServo = pos.x * 1000;
+                steeringServo = cropToRange(steeringServo, 0, 1000);
+                sendMessage("s=" + steeringServo);
             },
             endEvent: function (pos) {
-                console.log('Joystick 1:' + pos.x + ', ' + pos.y);
+                sendMessage("t=0");
+                
+                steeringServo = pos.x * 1000;
+                steeringServo = cropToRange(steeringServo, 0, 1000);
+                sendMessage("s=" + steeringServo);
             }
         });
         $('#stick2').joystick({
