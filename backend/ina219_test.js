c616f9dcfe307278d5a69caa67e908145e386ef5
Ralph <pi@rpi-robot>
2018-09-05 08:11:55 +0000

    ina219 test now takes readings once per second until killed.

--- backend/ina219_test.js
+++ backend/ina219_test.js
@@ -1,17 +1,23 @@
 var ina219 = require('ina219');
 
 ina219.init();
-ina219.enableLogging(true);
+// ina219.enableLogging(true);
 
 ina219.calibrate32V1A(function () {
-
-    ina219.getBusVoltage_V(function (volts) {
-
-        console.log("Voltage: " + volts);
-        ina219.getCurrent_mA(function (current) {
-
-            console.log("Current (mA): " + current);
-        });
-    });
+    setInterval(readStuff,1000);
 });
+
+function readStuff() {
+    ina219.getBusVoltage_V(function (volts) {
+        console.log((new Date().getTime() % 60000) + "Voltage: " + volts + "V");
+    });
+
+    ina219.getCurrent_mA(function (current) {
+        console.log((new Date().getTime() % 60000) + "Current: " + current + "mA");
+    });
+
+    ina219.getPower_mW(function (milliWatts) {
+	console.log((new Date().getTime() % 60000) + "Power: " + milliWatts + "mW");
+    });
+}
 
