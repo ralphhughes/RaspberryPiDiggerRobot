2a53d6ef965a679448eb2f664888d0cd7c38f5e6
Ralph <pi@rpi-robot>
2018-09-09 18:12:56 +0000

    Added detection for error thrown if ina219 is disconnected

--- backend/ina219_test.js
+++ backend/ina219_test.js
@@ -1,23 +1,23 @@
 var ina219 = require('ina219');
 
-ina219.init();
-// ina219.enableLogging(true);
+try {
+    ina219.init();
+    ina219.enableLogging(true);
 
-ina219.calibrate32V1A(function () {
-    setInterval(readStuff,1000);
-});
-
+    ina219.calibrate32V1A(function () {
+        setInterval(readStuff,1000);
+    });
+} catch (err) {
+    console.log(err.message);
+}
 function readStuff() {
     ina219.getBusVoltage_V(function (volts) {
-        console.log((new Date().getTime() % 60000) + "Voltage: " + volts + "V");
+        console.log((new Date().getTime() % 60000) + " Voltage: " + volts + "V");
     });
 
     ina219.getCurrent_mA(function (current) {
-        console.log((new Date().getTime() % 60000) + "Current: " + current + "mA");
+        console.log((new Date().getTime() % 60000) + " Current: " + current + "mA\n");
     });
 
-    ina219.getPower_mW(function (milliWatts) {
-	console.log((new Date().getTime() % 60000) + "Power: " + milliWatts + "mW");
-    });
 }
 
